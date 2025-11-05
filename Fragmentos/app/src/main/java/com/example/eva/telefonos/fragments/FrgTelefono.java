5bc4786f78b2f7238f714f65186625f14dbf0736
Yago Otero <Yagootero2003@gmail.com>
2025-11-05 22:06:37 +0100

    Terminado el ejericico de Fragments

--- Fragmentos/app/src/main/java/com/example/eva/telefonos/fragments/FrgTelefono.java
+++ Fragmentos/app/src/main/java/com/example/eva/telefonos/fragments/FrgTelefono.java
@@ -1,5 +1,6 @@
 package com.example.eva.telefonos.fragments;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,20 +17,31 @@ import androidx.fragment.app.Fragment;
 import com.example.eva.R;
 
 public class FrgTelefono extends Fragment {
+    onFrgTelefonoListener listener;
     String numTelefono;
+    String numTelefonoSaliente;
     boolean isCalling;
 
-    public interface frgTelefonoListener {
+    public interface onFrgTelefonoListener {
         /*
             boolean: valor de retorno, me dice si puedo llamar entrar en la llamada
          */
-         boolean onCall(FrgTelefono fragment, String numEntrante, String numSaliente);
-         void onColgar(FrgTelefono fragment, String numEntrante, String numSaliente);
+         boolean onCall(FrgTelefono fragment, String numSaliente);
+         void onColgar(FrgTelefono fragment, String numSaliente);
     }
 
     TextView txtNumTlfn;
     EditText edNumTlfn;
     ImageButton imgBtn;
+
+    public FrgTelefono(String numTelefono){
+        this.numTelefono = numTelefono;
+        isCalling = false;
+    }
+
+    // Porque hace falta uno vacío ? Bundle?
+    public FrgTelefono() {
+    }
 
     @Nullable
     @Override
@@ -39,6 +52,86 @@ public class FrgTelefono extends Fragment {
         edNumTlfn = view.findViewById(R.id.edNumTlfn);
         imgBtn = view.findViewById(R.id.imgBtn);
 
+        txtNumTlfn.setText(numTelefono);
+        imgBtn.setImageResource(R.drawable.call);
+        imgBtn.setOnClickListener(v -> llamar());
+
         return view;
+    }
+
+    public void setOnFrgTelefonoListener(onFrgTelefonoListener listener) {
+        this.listener = listener;
+    }
+
+    private void llamar(){
+        if (isCalling) {
+            listener.onColgar(this, numTelefonoSaliente);
+        } else if (!edNumTlfn.getText().toString().equals("")){
+            listener.onCall(this, edNumTlfn.getText().toString());
+        } else{
+            Toast.makeText(getActivity(), "No puedes llamar a un número vacío! ", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void empezarLlamada(String numSaliente){
+        setCalling(true);
+        setTxtNumTlfn(getNumTelefono().concat(" -> " + numSaliente));
+        setImgBtn(R.drawable.callend);
+        setNumTelefonoSaliente(numSaliente);
+        edNumTlfn.setText("");
+
+    }
+
+
+    public void terminarLlmada(){
+        setCalling(false);
+        setTxtNumTlfn(numTelefono);
+        setImgBtn(R.drawable.call);
+        setNumTelefonoSaliente(null);
+    }
+
+
+    public String getNumTelefono() {
+        return numTelefono;
+    }
+
+    public void setNumTelefono(String numTelefono) {
+        this.numTelefono = numTelefono;
+    }
+
+    public boolean isCalling() {
+        return isCalling;
+    }
+
+    public void setCalling(boolean calling) {
+        isCalling = calling;
+    }
+
+    public TextView getTxtNumTlfn() {
+        return txtNumTlfn;
+    }
+
+    public void setTxtNumTlfn(String txtNumTlfn) {
+        this.txtNumTlfn.setText(txtNumTlfn);
+    }
+
+    public EditText getEdNumTlfn() {
+        return edNumTlfn;
+    }
+
+    public void setEdNumTlfn(EditText edNumTlfn) {
+        this.edNumTlfn = edNumTlfn;
+    }
+
+    public void setImgBtn(int imgBtn) {
+        this.imgBtn.setImageResource(imgBtn);
+    }
+
+    public String getNumTelefonoSaliente() {
+        return numTelefonoSaliente;
+    }
+
+    public void setNumTelefonoSaliente(String numTelefonoSaliente) {
+        this.numTelefonoSaliente = numTelefonoSaliente;
     }
 }
--- Fragmentos/app/src/main/java/com/example/eva/telefonos/presentation/Telefonos.java
+++ Fragmentos/app/src/main/java/com/example/eva/telefonos/presentation/Telefonos.java
@@ -1,16 +1,31 @@
 package com.example.eva.telefonos.presentation;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.eva.R;
+import com.example.eva.telefonos.fragments.FrgTelefono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Telefonos extends AppCompatActivity {
+
+
+    List<FrgTelefono> telefonos = new ArrayList<>();
+    LinearLayout ll;
+    final int NUM_CONTACTOS = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +37,105 @@ public class Telefonos extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ll = findViewById(R.id.main);
+
+        iniciarContactos();
+
+    }
+
+    private void iniciarContactos() {
+        FragmentManager frgManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = frgManager.beginTransaction();
+
+        for (int i = 0; i < NUM_CONTACTOS; i++) {
+            //telefonoIndividual = (FrgTelefono) frgManager.findFragmentById(R.id.telefonoIndividual);
+            FrgTelefono telefonoIndividual = new FrgTelefono(String.valueOf(i));
+
+            telefonoIndividual.setOnFrgTelefonoListener(new FrgTelefono.onFrgTelefonoListener() {
+                @Override
+                public boolean onCall(FrgTelefono fragment, String numSaliente) {
+                    if (!comprobarDisponibilidad(fragment.getNumTelefono(), numSaliente)){
+                        return false;
+                    }
+
+                    if (fragment.getNumTelefono().equals(numSaliente)){
+                        Toast.makeText(Telefonos.this, "Que haces llamandote a ti mismo", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+
+                    telefonoIndividual.empezarLlamada(numSaliente);
+                    // 1: a quien busca, 2: cual añade (->)
+                    cambiarEstadoTelefono(numSaliente, fragment.getNumTelefono());
+                    return true;
+                }
+
+                @Override
+                public void onColgar(FrgTelefono fragment, String numSaliente) {
+                    telefonoIndividual.terminarLlmada();
+                    // 1: a quien busca, 2: cual añade (->)
+                    cambiarEstadoTelefono(numSaliente, fragment.getNumTelefono());
+                }
+            });
+
+            fragmentTransaction.add(R.id.main, telefonoIndividual, "telefono" + i);
+            telefonos.add(telefonoIndividual);
+        }
+
+        fragmentTransaction.commit();
+    }
+
+    private boolean comprobarDisponibilidad(String n1, String n2) {
+        for (int i = 0; i < telefonos.size(); i++) {
+        FrgTelefono tel = telefonos.get(i);
+            if (tel.getNumTelefono().equals(n1) && tel.isCalling()){
+                Toast.makeText(this, "El tlfn entrante se encuentra en una llamada", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            if (tel.getNumTelefono().equals(n2) && tel.isCalling()){
+                Toast.makeText(this, "El tlfn saliente se encuentra en una llamada", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void cambiarEstadoTelefono(String numTelefono, String numSaliente) {
+        for (int i = 0; i < telefonos.size(); i++) {
+            if (telefonos.get(i).getNumTelefono().equals(numTelefono)) {
+                if (telefonos.get(i).isCalling()) {
+                    telefonos.get(i).terminarLlmada();
+                } else {
+                    telefonos.get(i).empezarLlamada(numSaliente);
+                }
+                return;
+            }
+        }
+
+        Toast.makeText(this, "No se encontro el numero ", Toast.LENGTH_SHORT).show();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

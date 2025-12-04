56224456200d5fa77cfe135448b8871848ef0d48
Yago Otero <yagootero2003@gmail.com>
2025-12-04 14:30:04 +0100

    Ejercicios 04/12

---
+++ Concesionario/app/src/main/java/com/example/eva/ControllerMainActivity.java
@@ -0,0 +1,4 @@
+package com.example.eva;
+
+public class ControllerMainActivity {
+}
---
+++ Concesionario/app/src/main/java/com/example/eva/FrgBuscador.java
@@ -0,0 +1,6 @@
+package com.example.eva;
+
+import androidx.fragment.app.Fragment;
+
+public class FrgBuscador extends Fragment {
+}
